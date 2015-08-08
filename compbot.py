2e3fb4b21bd05a4f771d3e78f4ddd43c5329e699
Tzu-Yin Hong <lili668668@gmail.com>
2015-08-08 12:36:05 +0800

    解決刪除字句的問題

--- compbot.py
+++ compbot.py
@@ -1,13 +1,14 @@
 import random
-cust = input("孤單寂寞覺得冷嗎？電腦陪你說說話\n說明遊戲規則，如果你說「我說XXX，你說OOO」，就表示你正在教電腦說話，記住，電腦是隻猴子，你要教電腦說話他才會回答你。因為他是隻聰明的猴子，所以他會儲存對話內容。另外你可以試試，教他很多不一樣的回答，例如「我說蘋果，你說紅紅」「我說蘋果，你說好吃」「我說蘋果，你說超難吃」。如果你要刪除某像對話可以這樣說「你說ㄍㄢ，你壞壞」。\n事不宜遲，快來教跟這隻猴子講話吧\n")
+cust = input("孤單寂寞覺得冷嗎？電腦陪你說說話\n說明遊戲規則，如果你說「我說XXX，你說OOO」，就表示你正在教電腦說話，記住，電腦是隻猴子，你要教電腦說話他才會回答你。因為他是隻聰明的猴子，所以他會儲存對話內容。另外你可以試試，教他很多不一樣的回答，例如「我說蘋果，你說紅紅」「我說蘋果，你說好吃」「我說蘋果，你說超難吃」。如果你要刪除某像對話可以這樣說「我說蘋果,你說好吃，你壞壞」。\n事不宜遲，快來教跟這隻猴子講話吧\n")
 
 """範例"""
-a = ("好可愛","藍藍der")
+a = ["好可愛","藍藍der"]
 computer = { "哆拉A夢" : a }
 
 #主程式
 while True :
-    if "我說" in cust and "你說" in cust :
+    if "我說" in cust and ",你說" in cust and ",你壞壞" in cust :
+        print("有下去執行")
         (key,value) = (cust.split(",")[0],cust.split(",")[1])
         key = key.replace("我說","")
         value = value.replace("你說","")
@@ -15,20 +16,25 @@ while True :
             print("你沒有輸入定義喔")
         else :
             if key in computer :
-                computer[key]+=(value,)
+                if value in computer[key] :
+                    computer[key] = computer[key].remove(value)
+                    if computer[key] is None :
+                        del computer[key]
+                else :
+                    print("你沒有輸入過這句話喔")
             else :
-                computer[key] = (value,)
-    elif "你說" in cust and "你壞壞" :
-        """key = cust.split(",")[0]
-        key = key.replace("你說","")
-        if "" == key :
+                print("你沒有輸入過這句話喔")
+    elif "我說" in cust and ",你說" in cust :
+        (key,value) = (cust.split(",")[0],cust.split(",")[1])
+        key = key.replace("我說","")
+        value = value.replace("你說","")
+        if "" == key or "" == value :
             print("你沒有輸入定義喔")
         else :
             if key in computer :
-                del computer[key]
+                computer[key] += [value]
             else :
-                print("沒有定義過的項目無法刪除")"""
-        print("功能未全,銘謝惠顧")
+                computer[key] = [value]
     elif "" == cust :
         print("你並未輸入任何東西")
     else :
